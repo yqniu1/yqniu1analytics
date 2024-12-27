110687b2c610953a0c930710c7e31a9f9b06f1f2
pniu <31726381-pniu@users.noreply.replit.com>
2024-12-27 15:44:10 +0000

    Initial backend scaffold

--- app/main.py
+++ app/main.py
@@ -3,7 +3,10 @@ from fastapi import FastAPI
 #create application instance
 app = FastAPI()
 
+
 #define endpoint for GET request
 @app.get("/")
 def read_root():
-  return {"message":"Welcome to the analytics backend."}
+  return {"message": "Welcome to the analytics backend."}
+
+read_root()
